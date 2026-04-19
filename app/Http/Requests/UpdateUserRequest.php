53969638382162bce65ba7c4e039679f3868283e
sazzadursiam <mdsazzadur.rahman@sslwireless.com>
2026-04-19 14:15:11 +0600

    add HTTP requests and UserResource for user management API

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $userId = $this->route('user')->id;
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'email',
+                'max:255',
+                Rule::unique('users', 'email')->ignore($userId)
+            ],
+            'password' => ['nullable', 'string', 'min:6']
+        ];
+    }
+}
