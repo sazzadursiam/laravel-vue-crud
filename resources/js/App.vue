045cba9b6a116a17f1a738887f6f06796747edef
sazzadursiam <mdsazzadur.rahman@sslwireless.com>
2026-04-19 12:48:16 +0600

    add navbar and layout structure to App.vue

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <router-link class="navbar-brand" to="/">Laravel Vue CRUD</router-link>
+            </div>
+        </nav>
+        <div class="container mt-4">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
